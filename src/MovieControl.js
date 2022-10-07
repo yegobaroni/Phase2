4814faa8e07c639db050e27ae00b9fe116a3f048
yego <yegokipkemoi254@gmail.com>
2022-10-08 01:14:20 +0300

    created events and passed props to them

---
+++ src/MovieControl.js
@@ -0,0 +1,27 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from "../context/GlobalState";
+
+function MovieControls({movie, type}) {
+
+    const {removeMovieFromWatchList, addMovieToWatched, removeFromWatched} = useContext(GlobalContext);
+  return (
+    // 
+    <div >
+        {type === 'watchList' && (
+            <>
+            <button onClick={() => addMovieToWatched(movie)}>Watched</button>
+            <button onClick={() => removeMovieFromWatchList(movie.id)}>Remove</button>
+            </>
+        )}
+
+        {type === "watched" && (
+            <>
+            <button onClick={() => removeFromWatched(movie.id)}>Remove</button>
+            </>
+
+        )}
+    </div>
+  )
+}
+
+export default MovieControls
