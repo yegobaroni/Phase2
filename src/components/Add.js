868362019732715636bec86bf4536d11a0c4289b
yego <yegokipkemoi254@gmail.com>
2022-10-08 01:09:22 +0300

    displayed movie card

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,15 +1,15 @@
-// import React, {useState} from 'react';
-// import ResultsCard from './ResultsCard';
+import React, {useState} from 'react';
+import ResultsCard from './ResultsCard';
 
 function Add() {
 
-  //const [query, setQuery] = useState("");
-  // const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
 
 
-  // const handleChange = e =>{
-  //   e.preventDefault();
-  //   setQuery(e.target.value);
+  const handleChange = e =>{
+    e.preventDefault();
+    setQuery(e.target.value);
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
     .then((res) => res.json())
@@ -22,14 +22,14 @@ function Add() {
     });
   };
   return (
-    <div className='add-page'>
+    <div>
       <div className='input-wrapper'>
       <input type="text" 
       placeholder='Search for a Movie'
-      // value={query}
-      // onChange={handleChange}
+      value={query}
+      onChange={handleChange}
       /></div>
-      {/* {results.length > 0 && (
+      {results.length > 0 && (
         <ul className='results'>
           {results.map(movie =>(
             <li key={movie.id}>
@@ -37,7 +37,7 @@ function Add() {
             </li>
           ))}
         </ul>
-      )} */}
+      )}
     </div>
   )
 }
