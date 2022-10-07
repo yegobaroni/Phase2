ed754fadb6654fe16ed553bf9b7a325f2d45060b
yego <yegokipkemoi254@gmail.com>
2022-10-07 15:32:20 +0300

    fetched data from api

--- src/components/Add.js
+++ src/components/Add.js
@@ -11,16 +11,16 @@ function Add() {
   //   e.preventDefault();
   //   setQuery(e.target.value);
 
-  //   fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     if(!data.errors){
-  //       setResults(data.results);
-  //     } else {
-  //       setResults([]);
-  //     }
-  //   });
-  // };
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if(!data.errors){
+        setResults(data.results);
+      } else {
+        setResults([]);
+      }
+    });
+  };
   return (
     <div className='add-page'>
       <div className='input-wrapper'>
